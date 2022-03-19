d80c086ee73bc8794b3dce0e64f2168effe68070
oyesa <mercyoyesa2018@gmail.com>
2022-03-19 18:24:52 +0300

    add function for proceed button

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,15 @@ function getPizza(name,size,crust,topping,total){
   this. total = total;
 }
 
+// Function for proceed button
+$("button.proceed").click(function(event){
+  let pName = $(".name option:selected").val();
+  let pSize = $("#size option:selected").val();
+  let pCrust = $("#crust option:selected").val();
+  let pTopping = [];
+
+  $.each($("input[name='toppings']:checked"), function(){
+    pTopping.push($(this).val());
+  });
+  console.log(pTopping.join(", "));
+})
