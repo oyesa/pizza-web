8f34212edfb8286524cbc36bfeac044321d7b889
oyesa <mercyoyesa2018@gmail.com>
2022-03-19 18:11:27 +0300

    update changes and add constructor

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+
+
+
+var price, crust_price, topping_price;
+let total = 0;
+
+function getPizza(name,size,crust,topping,total){
+  this.name = name;
+  this.size = size;
+  this.crust = crust;
+  this.topping = topping;
+  this. total = total;
+}
+
