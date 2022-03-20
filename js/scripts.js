b5798c7a9de73a5f0b295b142bd348328d3d5315
oyesa <mercyoyesa2018@gmail.com>
2022-03-20 09:25:43 +0300

    add user logic

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@
 var price, crust_price, topping_price;
 let total = 0;
 
-function getPizza(name,size,crust,topping,total){
+function Getpizza(name,size,crust,topping,total){
   this.name = name;
   this.size = size;
   this.crust = crust;
@@ -24,3 +24,45 @@ $("button.proceed").click(function(event){
   });
   console.log(pTopping.join(", "));
 })
+
+switch(pSize){
+  case "0":
+    price = 0;
+    break;
+
+    case "large":
+      price = 1500;
+      console.log(price);
+      break;
+
+      case "medium":
+        price = 1000;
+        console.log("The price is "+price);
+        break;
+
+        case "small":
+          price = 700;
+          console.log(price);
+          default:
+            console.log("error");
+}
+
+switch(pCrust){
+  case "0":
+    crust_price = 0;
+    break;
+
+    case "Crispy":
+      crust_price = 300;
+      break;
+
+      case "Stuffed":
+        crust_price = 250;
+        break;
+
+        case "Gluten-free":
+          crust_price = 200;
+          break;
+          default:
+            console.log("No Price");
+}
