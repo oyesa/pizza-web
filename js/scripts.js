0da0071e75497efed605335335b9cdbfaf42b5fb
oyesa <mercyoyesa2018@gmail.com>
2022-03-20 10:08:15 +0300

    add function for add pizza button

--- js/scripts.js
+++ js/scripts.js
@@ -19,7 +19,7 @@ $("button.proceed").click(function(event){
   let pCrust = $("#crust option:selected").val();
   let pTopping = [];
 
-  $.each($("input[name='toppings']:checked"), function(){
+  $.each($("input[name='Toppings']:checked"), function(){
     pTopping.push($(this).val());
   });
   console.log(pTopping.join(", "));
@@ -94,3 +94,54 @@ $("#pizzatopping").html(pTopping.join(", "));
 $("#totals").html(total);
 
 // Add button function
+
+$("button.addPizza").click(function(){
+  let pName = $(".name option:selected").val();
+  let pSize = $("#size option :selected").val();
+  let pCrust = $("#crust option :selected").val();
+  let pTopping = [];
+  $.each($("input[name='Toppings']: checked"), function(){
+    pTopping.push($(this).val());
+  });
+  console.log(pTopping.join(", "));
+  switch(pSize){
+    case "0":
+      price = 0;
+      break;
+
+      case "large":
+        price = 1500;
+        console.log(price);
+        break;
+
+        case "medium":
+          price = 1000;
+          console.log("The price is "+price);
+          break;
+
+          case "small":
+            price = 700;
+            console.log(price);
+            default:
+              console.log("error");
+  }
+  switch(pCrust){
+    case "0":
+      crust_price = 0;
+      break;
+
+      case "Crispy":
+        crust_price = 300;
+        break;
+
+        case "Stuffed":
+          crust_price = 250;
+          break;
+
+          case "Gluten-free":
+            crust_price = 200;
+            break;
+            default:
+              console.log("No crust");
+  }
+});
